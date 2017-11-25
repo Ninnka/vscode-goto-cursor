7ad7a5427186d180bd6bd797d209f257bbcb4efc
Rennzh <hennzr@gmail.com>
2017-11-26 00:07:08 +0800

    first release

---
+++ README.md
@@ -0,0 +1,33 @@
+# goto-cursor README
+
+This is the README for your extension "goto-cursor". After writing up a brief description, we recommend including the following sections.
+
+## Features
+
+move the panel positioning to cursor point quickly
+
+> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+
+## Requirements
+
+no any requirements
+
+## Extension Settings
+
+no any extension settings
+
+## Known Issues
+
+no any issues
+
+## Release Notes
+# 0.0.1
+first release
+
+-----------------------------------------------------------------------------------------------------------
+
+### For more information
+
+* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,46 @@
+'use strict';
+
+import * as vscode from 'vscode';
+import { Selection } from 'vscode';
+
+// * 准备变量
+let editor: vscode.TextEditor;
+let selection: vscode.Selection;
+
+// * 更新所在行和列
+const revealLine = (line: number = 0, col: number = 0) => {
+    let reviewType: vscode.TextEditorRevealType = vscode.TextEditorRevealType.InCenter;
+    if (line === vscode.window.activeTextEditor.selection.active.line) {
+        reviewType = vscode.TextEditorRevealType.InCenterIfOutsideViewport;
+    }
+    let newSe = new vscode.Selection(line, col, line, col);
+    vscode.window.activeTextEditor.selection = newSe;
+    vscode.window.activeTextEditor.revealRange(newSe, reviewType);
+}
+
+// * this method is called when your extension is activated
+// * your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+
+    editor = vscode.window.activeTextEditor;
+    if (editor) {
+        selection = editor.selection;
+    }
+
+    let useGotoCursor: vscode.Disposable = vscode.commands.registerCommand('extension.gotoCursor', () => {
+        let lineSaved: number = selection && selection.active ? selection.active.line : 0;
+        let colSaved: number = selection && selection.active ? selection.active.character + 1 : 0;
+        revealLine(lineSaved, colSaved);
+    });
+
+    context.subscriptions.push(useGotoCursor);
+}
+
+// * this method is called when your extension is deactivated
+export function deactivate() {}
